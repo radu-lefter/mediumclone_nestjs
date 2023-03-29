1247e9cabf9b5239216063f8472898cb42684e60
raduubraduu <lefter_radu@yahoo.com>
2023-03-29 17:26:43 +0100

    created the tag entity and configured ormconfig to create table

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,16 @@
-import { ConnectionOptions } from 'typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import {TagEntity} from './tag/tag.entity'
 
-const config: ConnectionOptions = {
+const config: PostgresConnectionOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'mediumclone',
   password: 'Havefun1!',
   database: 'mediumclone',
+  //entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  entities: [TagEntity],
+  synchronize: true,
 };
 
 export default config;
