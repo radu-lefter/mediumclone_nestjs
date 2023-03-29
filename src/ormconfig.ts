4dcac141486aca048fbdc6fad98161c1edefa9f9
raduubraduu <lefter_radu@yahoo.com>
2023-03-29 16:50:37 +0100

    configured and binded to postgres database

---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumclone',
+  password: 'Havefun1!',
+  database: 'mediumclone',
+};
+
+export default config;
