57e18addd717ce1314b29a7614d7decc2dc07516
raduubraduu <lefter_radu@yahoo.com>
2023-03-30 20:08:54 +0100

    implmented validation pipe for user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
-import {CreateUserDto} from './dto/createUser.dto';
+import { CreateUserDto } from './dto/createUser.dto';
 import { UserResponseInterface } from './types/userResponse.interface';
 
 @Controller()
@@ -8,7 +14,10 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
+  @UsePipes(new ValidationPipe())
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
     const user = await this.userService.createUser(createUserDto);
     return this.userService.buildUserResponse(user);
   }
