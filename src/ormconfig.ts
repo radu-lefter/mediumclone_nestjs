d8a8008efa4ef13addc9c3e5c80b3ab33b947f8c
raduubraduu <lefter_radu@yahoo.com>
2023-03-30 19:24:19 +0100

    downgraded to typeorm 3.11 in order to solve migration problems

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,5 +1,6 @@
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 import {TagEntity} from './tag/tag.entity'
+import {UserEntity} from './user/user.entity'
 
 const config: PostgresConnectionOptions = {
   type: 'postgres',
@@ -8,8 +9,8 @@ const config: PostgresConnectionOptions = {
   username: 'mediumclone',
   password: 'Havefun1!',
   database: 'mediumclone',
-  //entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  entities: [TagEntity],
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  //entities: [TagEntity, UserEntity],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
 };
