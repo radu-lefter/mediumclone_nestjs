8482b0d93b92af1679863bb8e690dc151ef36541
raduubraduu <lefter_radu@yahoo.com>
2023-04-01 16:51:37 +0100

    updated article controller for create function

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { User } from '@app/user/decorators/user.decorator';
+import { UserEntity } from '@app/user/user.entity';
+import { CreateArticleDto } from './dto/createArticle.dto';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
   @Post()
-  async create() {
-    return this.articleService.createArticle();
+  @UseGuards(AuthGuard)
+  async create(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<any> {
+    return this.articleService.createArticle(currentUser, createArticleDto);
   }
 }
