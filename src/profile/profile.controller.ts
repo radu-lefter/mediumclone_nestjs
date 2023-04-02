a2a7e3aebca503ca559f359a6b7a6f2636b094e3
raduubraduu <lefter_radu@yahoo.com>
2023-04-02 19:32:51 +0100

    implemented unfollow functionality

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,5 +1,5 @@
 import { User } from '@app/user/decorators/user.decorator';
-import { Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
 import {ProfileService} from './profile.service';
 import { ProfileResponseInterface } from './types/profileResponse.interface';
 import { AuthGuard } from '@app/user/guards/auth.guard';
@@ -27,6 +27,19 @@ export class ProfileController {
     @Param('username') profileUsername: string,
   ): Promise<ProfileResponseInterface> {
     const profile = await this.profileService.followProfile(
+      currentUserId,
+      profileUsername,
+    );
+    return this.profileService.buildProfileResponse(profile);
+  }
+
+  @Delete(':username/follow')
+  @UseGuards(AuthGuard)
+  async unfollowProfile(
+    @User('id') currentUserId: number,
+    @Param('username') profileUsername: string,
+  ): Promise<ProfileResponseInterface> {
+    const profile = await this.profileService.unfollowProfile(
       currentUserId,
       profileUsername,
     );
